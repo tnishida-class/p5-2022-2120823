891962ab2f305b7e5cb34ef47802aa221c86dcc0
2120823 <114970234+2120823@users.noreply.github.com>
2022-10-17 23:25:46 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,14 +1,26 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  fill(128);
   let size = width / 8;
+  background(255);
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      if(i % 2 == 0 && j % 2 == 0){  
+        fill(0);
+      }
+      else if(i % 2 == 0 && j % 2 == 1){  
+        fill(255);
+      }
+      else if(i % 2 == 1 && j % 2 == 0){  
+        fill(255);
+      }
+      else if(i % 2 == 1 && j % 2 == 1){  
+        fill(0);
+      }
+
       
-      console.log(i , j);
-      lect(i * 8, j * 8, 25, 25);
+      lect(i * 25, j * 25, 25, 25);
         
       
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
