af29741ad089a1a74079635087c06824dfe97018
2120823 <114970234+2120823@users.noreply.github.com>
2022-10-17 15:35:53 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,9 +5,8 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      fill(i + j % 2 == 0 ? 255 : 0)
       console.log(i , j);
-      lect(i * 8, j * 8, 10, 10);
+      lect(i * 8, j * 8, 25, 25);
         
       
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
