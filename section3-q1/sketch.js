25d2c3b0f446c01c942fa4718ed69b91cfc5caab
2120823 <114970234+2120823@users.noreply.github.com>
2022-10-18 12:03:31 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,6 +1,7 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let vi;
 
 function setup(){
   createCanvas(400, 400);
@@ -12,6 +13,15 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   console.log(count);
+  if(count < 10){
+    let size = count * 3 + 50;
+  }
+  else if(count > 10 && count < 40){
+    let size = (count - 10) * (-1) + 80;
+  }
+  else if(count > 40 && count < 60){
+    let size = 50;
+  }
   // BLANK[1]
   let size = count + 50;
   fill(220, 20, 60)
