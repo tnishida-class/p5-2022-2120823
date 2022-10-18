21a9b63737fffee8fc81ba3510565b93009fb507
2120823 <114970234+2120823@users.noreply.github.com>
2022-10-18 11:35:24 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -3,14 +3,17 @@ let count;
 let cycle;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   count = 0;
-  cycle = 100;
+  cycle = 45;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
+  console.log(count);
   // BLANK[1]
+  let size = count + 50;
+  fill(220, 20, 60)
   ellipse(width / 2, height / 2, size);
 }
