002da4f7fdd3f5c6cba0cb1c4fb56e2343220633
2120823 <114970234+2120823@users.noreply.github.com>
2022-10-20 14:07:04 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -2,29 +2,41 @@
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
-  background(255);
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      if(i % 2 == 0 && j % 2 == 0){  
-        fill(0);
+      if((i + j) % 2 == 1){  
+        fill(180);
       }
-      else if(i % 2 == 0 && j % 2 == 1){  
+      else {  
         fill(255);
       }
-      else if(i % 2 == 1 && j % 2 == 0){  
-        fill(255);
-      }
-      else if(i % 2 == 1 && j % 2 == 1){  
-        fill(0);
-      }
-
-      console.log(i , j);
-      lect(i * 25, j * 25, 25, 25);
+      rect(i * 20, j * 20, 20, 20);
         
       
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       
     }
   }
+}
+
+function draw(){
+  fill(255, 0, 0);
+  let x = 30
+  while (x < 160){
+    ellipse(x, 10, 15)
+    x = x + 40
+  }
+  let y = 10
+  while (y < 160){
+    ellipse(y, 30, 15)
+    y = y + 40
+  }
+  let a = 30
+  while (a < 160){
+    ellipse(a, 10, 15)
+    a = a + 40
+  }
+
+  
 }
