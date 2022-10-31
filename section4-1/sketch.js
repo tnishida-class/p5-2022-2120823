b8e75bd97fb27b1cef6387f4c213e3f567ccfd5a
2120823 <114970234+2120823@users.noreply.github.com>
2022-10-31 14:41:34 +0900

    Update sketch.js

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -20,8 +20,8 @@ function setup(){
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
-  for(let i = 0; i < 10; i ++){
-    average= sum / 10;
+  for(let i = 0; i < scores.length; i ++){
+    average= sum / scores.length;
   }
   console.log(average);
 
@@ -70,7 +70,5 @@ function setup(){
   const ay = height - height * average / 100;
   stroke(0,255,0);
   line(0, ay, width, ay);
-  stroke(0);
-  fill(0);
-  text(average.toPrecision(3), i * dx, height - h);
+  text(average.toPrecision(3), 0, height - average);
 }
